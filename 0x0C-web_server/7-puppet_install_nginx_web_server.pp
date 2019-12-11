798d5a5275d6d79fe9d3e24541b3106c611e4c27
edward0rtiz <931@holbertonschool.com>
2019-12-11 14:00:19 -0500

    [fix] move to sites available

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,14 +2,14 @@
 
 package {'nginx':
   ensure => 'present',
-}
--> exec {'Holberton':
+}->
+exec {'Holberton':
   command => '/bin/echo "Holberton School" > /var/www/html/index.html',
   returns => [0,2]
-}
--> exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301h https:\/\/www.youtube.com\/watch?v=xJJsoquu70o\/;\\n\\t}/" /etc/nginx/sites-enabled/default':
+}->
+exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301h https:\/\/www.youtube.com\/watch?v=xJJsoquu70o\/;\\n\\t}/" /etc/nginx/sites-available/default':
   path => '/usr/bin:/usr/sbin:/bin',
-}
--> exec {'run':
+}->
+exec {'run':
   command => '/usr/sbin/service nginx start',
 }
