a389edc27944cc6c97d0e9ea5a9f45a68193394a
edward0rtiz <931@holbertonschool.com>
2019-12-11 14:17:57 -0500

    [fix] test rewrite using more commands

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,11 +8,11 @@ file {'/var/www/html/index.html':
   content => 'Holberton School'
 }
 
-file_line {'title':
+file_line { 'title':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com\/watch?v=xJJsoquu70o;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
 }
 
 service {'nginx':
