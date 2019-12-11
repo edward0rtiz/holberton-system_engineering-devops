e76a2c1d7e4f5667de441ec5d7f7b9b7bc1ae667
edward0rtiz <931@holbertonschool.com>
2019-12-11 14:48:49 -0500

    [fix] new line

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,20 +2,25 @@
 
 package {'nginx':
   ensure => 'installed',
-}
+  ensure => 'present',
+}->
 
 file {'/var/www/html/index.html':
   content => 'Holberton School'
-}
+}->
 
 file_line { 'title':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
   line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
-}
+}->
 
 service {'nginx':
   ensure  => 'running',
   require => 'Package['nginx'],
+}->
+
+exec {'run':
+  command => 'usr/sbin/service nginx start',
 }
