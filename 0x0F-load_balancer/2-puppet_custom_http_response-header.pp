06c7afbd121ba70d6d39355fb6b0448cd9e9f28b
edward0rtiz <931@holbertonschool.com>
2020-01-02 22:08:54 -0500

    [feature] puppet script task 0

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,16 @@
+#automate task 0 but with Puppet
+
+exec {'update':
+  command => '/usr/bin/apt-get update'},
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
