b3a4db70334cb7832db9e62f8198601b90d6bd90
edward0rtiz <931@holbertonschool.com>
2020-02-03 14:27:28 -0500

    [feature] export to csv WIP

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script to export data in the CSV format."""
+
+import csv
+import requests as r
+import sys
+
+if __name__ == '___main__':
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    usr = r.get(url + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    to_do = r.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow([user_id, username, title.get("completed"),
+                          title.get("title")]) for title in to_do]
