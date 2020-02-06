783774885ac6c994a98a39352da969f79279e749
edward0rtiz <931@holbertonschool.com>
2020-02-06 14:27:07 -0500

    [fix] pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,10 +2,12 @@
 
 import requests as r
 
+
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
     }
     response = r.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
