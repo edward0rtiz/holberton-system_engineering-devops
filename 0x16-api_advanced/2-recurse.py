42dee76c41fb4deaca323a303964253ae2f27531
edward0rtiz <931@holbertonschool.com>
2020-02-06 15:41:00 -0500

    [Feature] testing get using hot in recursive method

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests as r
+
+
+def recurse(subreddit, hot_list=[], after="Null"):
+    url = "https://www.reddit.com/r/{}/hot.json()".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+        }    
+    param = {
+        "after": after,
+        "limit": 100,
+    }
+    response = r.get(url, headers=headers, params=param,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    else:
+        posts = hot.json().get("data").get("children")
+        hot_list += [post.get("data").get("title") for post in posts]
+        after = hot.json().get("data").get("after")
+        if after is not None:
+                recurse(subreddit, hot_list, after)
+        return hot_list
